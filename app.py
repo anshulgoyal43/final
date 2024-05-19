b9bfd47e0ba2b1badebaa0c1bee779ff13d4c8e1
anshulgoyal43 <65175818+anshulgoyal43@users.noreply.github.com>
2024-05-20 00:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,6 @@ def upload():
         return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
